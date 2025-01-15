a58ace989c1d67fa12f5fe3b12b7d7a3ae3bdff8
Dekdum020 <natthawut.pr@rmutsvmail.com>
2025-01-15 09:48:26 +0700

    KFC

--- contact.php
+++ contact.php
@@ -13,10 +13,10 @@
                 <img src="images/map.gif">
             </td>
             <td width="190" bgcolor="#FFFF66"><ul>
-				<li><a href="home.php">หน้าแรก</li>
-				<li><a href="room.php">ห้องพัก&ราคา</li>
-				<li><a herf="guest.php">สมุดเยี่ยม</li>
-				<li><a href="contact.php">ติดต่อ</li>
+				<li><a href="home.php">หน้าแรกKFC</li>
+				<li><a href="room.php">ห้องพัก&ราคาKFC</li>
+				<li><a herf="guest.php">สมุดเยี่ยมKFC</li>
+				<li><a href="contact.php">ติดต่อKFC</li>
 				</ul><p>
 				<?php include "login_form.php";?>
 			</td>
